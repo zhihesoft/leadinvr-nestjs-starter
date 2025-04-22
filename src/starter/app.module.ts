7b6743d502dce29fb6cc75e67c09a673aaebc495
stanley <houyuqing@hotmail.com>
2025-04-22 13:28:32 +0800

    add revoke token check

--- src/starter/app.module.ts
+++ src/starter/app.module.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 import { DynamicModule } from "@nestjs/common";
 import { CacheModule } from "../cache/cache.module";
+import { CacheRevokeTokenService } from "../cache/lib/cache.revoke.token.service";
 import { CommonModule } from "../common/common.module";
 import { PackageInfoDto } from "../common/domain/package.info.dto";
 import { JwtGuardModule } from "../jwt.guard/jwt.guard.module";
@@ -16,10 +17,17 @@ export class AppModule {
                 CommonModule,
                 LoggerModule.register(true),
                 CacheModule.register({ global: true, redisUri: process.env.REDIS_URI ?? "", workspace: pkg.name }),
-                JwtGuardModule.register({
+                JwtGuardModule.registerAsync({
                     isGlobal: true,
                     autoRegister: true,
-                    secret: process.env.JWT_SECRET,
+                    inject: [CacheRevokeTokenService],
+                    useFactory: (revokes: CacheRevokeTokenService) => ({
+                        secret: process.env.JWT_SECRET,
+                        checker: async token => {
+                            const result = await revokes.isTokenRevoked(token);
+                            return !result;
+                        },
+                    }),
                 }),
                 ...(options.imports ?? []),
             ],
