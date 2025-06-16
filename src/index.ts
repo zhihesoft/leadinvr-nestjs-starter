ef4865bd4a5cbd17d21ee02aee73229ba1e13b16
stanley <houyuqing@hotmail.com>
2025-06-16 14:23:05 +0800

    update export

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ export * from "@leadinvr/common";
 export * from "@leadinvr/jwt-guard";
 export * from "@leadinvr/logger";
 
-export { AppOption, startup } from "./lib/starter";
+export { startup } from "./lib/starter";
+export { StarterModule } from "./lib/starter.module";
+export { StarterModuleOptions } from "./lib/starter.module.options";
+export { StarterOption } from "./lib/starter.option";
 
